3cd0cf5535f3f4dbd7f0baf8c9fb1b94b318d964
Mashmighty <110117092+Mashmighty@users.noreply.github.com>
2023-05-09 21:52:38 +0300

    create form components

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import Header from './components/header'
 import SideMenu from './components/sideMenu';
 import Tabs from './components/tabs';
 import Select from './components/select';
+import myForm from './components/formQuestionnaire';
 function App() {
   const tabs= [{name:"Home"},{name:"Editions"},{name:"Individual agent report"},{name:"Individual shop report"},{name:"All shops report"}]
-  const options= [{name:"January",value:"January"},{name:"February",value:"February"},{name:"March",value:"March"},{name:"April",value:"April"},{name:"May",value:"May"}]
-  const years= [{name:"2023",value:"2023"}]
+  const options= [{name:"January",value:"January"},{name:"February",value:"February"},{name:"March",value:"March"},{name:"April",value:"April"},{name:"May",value:"May"},
+  {name:"June",value:"June"},{name:"July",value:"July"},{name:"August",value:"August"},{name:"September",value:"September"},{name:"October",value:"October"},{name:"November",value:"November"},
+  {name:"December",value:"December"}]
+  const years= [{name:"2023",value:"2023"},{name:"2024",value:"2024"}]
   return (
     <div className="App">
      <Header/>
---
+++ src/components/formQuestionnaire/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./index.css"
+const myForm = () => {
+  return (
+    <div>myForm</div>
+  )
+}
+
+export default myForm
